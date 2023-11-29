a5ce2fbbf0d85eb5588566c21bca8297623a2081
rekun <rekun@LAPTOP-JS11QQ90>
2023-11-29 14:56:16 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+ int n = 1;
+ Thread thr1;
+    thr1 = new Thread(d);
+    thr1.Start();
+
+    // Ожидание завершения потока
+    thr1.Join();
+
+    Console.ReadLine();
+
+
+static void d(object size)
+{
+    Stopwatch sw = new Stopwatch();
+    sw.Start();
+
+    string stri1;
+    stri1 = "мама, папа, и никита";
+    stri1 = stri1.Replace(" ", "<>");
+    Console.WriteLine(stri1);
+
+    sw.Stop();
+    Console.WriteLine("Stopwatch: " + sw.ElapsedTicks.ToString());
+}
